919ea6e8bf1680b9af01904b94cbfdef4e134d75
ikatsuki <ik2721@gmail.com>
2015-03-29 19:14:29 +0900

    Add new page that we create place memo

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,4 +2,7 @@ class MemosController < ApplicationController
   def index
     @memo_items = Memo.all
   end
+
+  def new 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'memos#index'
-  get 'memos/index'
+  resources :memos, only: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
