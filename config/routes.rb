796f2cf724f458069ffb5d640ade64cf08bee884
ikatsuki <ik2721@gmail.com>
2015-04-07 23:49:19 +0900

    Add memo source

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'memos#index'
-  resources :memos, only: [:index, :new]
+  resources :memos
+  resources :places
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
