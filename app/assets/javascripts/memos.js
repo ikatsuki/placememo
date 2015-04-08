ab329f0f449953ee6bd27c9d45285178c53d8ce4
ikatsuki <ik2721@gmail.com>
2015-04-08 23:45:33 +0900

    Refactoring memos.js

--- app/assets/javascripts/memos.js
+++ app/assets/javascripts/memos.js
@@ -1,27 +1,13 @@
 window.onload = function()
 {
   // Get present location
-  getLocation();
+  pm_map.getLocation();
 
   // After place memo insert
   $('#memo_detail_places').on('cocoon:after-insert', function(e, insertedItem) {
     $(insertedItem).collapsible();
 
-    // Set marker on present location
-    var marker = new google.maps.Marker({
-      map: map,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
-
-    // Set autocomplete to address input field 
-    $(insertedItem).find('.address').each(function(i,e){
-      var autocomplete = new google.maps.places.Autocomplete($(this)[0]);
-      autocomplete.bindTo('bounds', map);
-
-      google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        redrawGoogleMap(map, marker, autocomplete);
-      });
-    });
+    pm_map.setAutoComplete(insertedItem);
   });
 
   // Disable enter key in form 
@@ -31,146 +17,142 @@ window.onload = function()
 
 };
 
-var map;
-var markers = [];
 
-/**
- * Initialize Google Map
- * @param {Number} x latitude
- * @param {Number} y longitude 
- * @return {}
- */
-function initialize(x, y) {
+var pm_map = pm_map || {}; // ancestor namespace
 
-  map = new google.maps.Map(
+(function (pm_map) { // namespace
+
+  pm_map.map = (function () {
+    return {
+    };
+  })();
+
+
+  /**
+   * Initialize Google Map
+   * @param {Number} x latitude
+   * @param {Number} y longitude 
+   * @return {}
+   */
+  function initialize(x, y) {
+
+    pm_map.map = new google.maps.Map(
       document.getElementById('map_canvas'), 
       setOption(x,y));
 
-    // var input = document.getElementById('place_input');
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    pm_map.setAutoComplete(null);
+  }
 
-    // var search_btn = document.getElementById('search_btn');
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(search_btn);
-    //
-    // var add_btn = document.getElementById('add_btn');
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(add_btn);
+  pm_map.setAutoComplete = function (insertedItem) {
+ 
+    var $top = insertedItem ? $(insertedItem) : $('#contents');
 
     // Set marker on present location
     var marker = new google.maps.Marker({
-      map: map,
+      map: pm_map.map,
       anchorPoint: new google.maps.Point(0, -29)
     });
-    marker.setPosition(new google.maps.LatLng(x, y));
 
     // Set autocomplete to address input field 
-    $('.address').each(function(i,e){
+    $top.find('.address').each(function(i,e){
       var autocomplete = new google.maps.places.Autocomplete($(this)[0]);
-      autocomplete.bindTo('bounds', map);
+      autocomplete.bindTo('bounds', pm_map.map);
 
       google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        redrawGoogleMap(map, marker, autocomplete);
+        pm_map.redrawGoogleMap(marker, autocomplete);
       });
     });
+  
+  }
 
-    // var input = document.getElementById('form_memo_places_attributes_0_address');
-    // var autocomplete = new google.maps.places.Autocomplete(input);
-    // autocomplete.bindTo('bounds', map);
+  /**
+   * Set options for Google Map
+   * @param {Number} x latitude
+   * @param {Number} y longitude 
+   * @return {Object} myOptions options for google map
+   */
+  function setOption(x, y){
+    var myOptions = {
+      zoom: 17,
+      center: new google.maps.LatLng(x, y),
+      mapTypeId: google.maps.MapTypeId.ROADMAP, 
+      disableDefaultUI: true,
+      panControl: true,
+      panControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      }
+    };
+    return myOptions;
+  }
 
+  /**
+   * Create LatLng object 
+   * @param {Number} x  latitude
+   * @param {Number} y  longitude 
+   * @return {Object} latlng  LatLng object 
+   */
+  function setLatlng(x, y){
+    var latlng = new google.maps.LatLng(x, y);
+    return latlng;
+  }
 
-    // Add place changed event 
-}
+  /**
+   * Get current location
+   * @param {} 
+   * @return {} 
+   */
+  pm_map.getLocation = function (){
+    navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+  }
 
-/**
- * Set options for Google Map
- * @param {Number} x latitude
- * @param {Number} y longitude 
- * @return {Object} myOptions options for google map
- */
-function setOption(x, y){
-  var myOptions = {
-    zoom: 17,
-    center: new google.maps.LatLng(x, y),
-    mapTypeId: google.maps.MapTypeId.ROADMAP, 
-    disableDefaultUI: true,
-    panControl: true,
-    panControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
-    },
-    zoomControl: true,
-    zoomControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
+  /**
+   * getCurrentPosition function succeeded in getting current location 
+   * @param {object} pos   
+   * @return {} 
+   */
+  function successCallback(pos){
+    var Potition_latitude = pos.coords.latitude;
+    var Potition_longitude = pos.coords.longitude;
+
+    // 位置情報が取得出来たらGoogle Mapを表示する
+    initialize(Potition_latitude,Potition_longitude);
+  }
+
+  /**
+   * getCurrentPosition function failed in getting current location 
+   * @param {object} error   
+   * @return {} 
+   */
+  function errorCallback(error){
+  }
+
+  /**
+   * Redraw map
+   * @param {object} error   
+   * @return {} 
+   */
+  pm_map.redrawGoogleMap = function (marker,autocomplete){
+
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) return;
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      pm_map.map.fitBounds(place.geometry.viewport);
+    } else {
+      pm_map.map.setCenter(place.geometry.location);
+      pm_map.map.setZoom(17);
     }
-  };
-  return myOptions;
-}
 
-/**
- * Create LatLng object 
- * @param {Number} x  latitude
- * @param {Number} y  longitude 
- * @return {Object} latlng  LatLng object 
- */
-function setLatlng(x, y){
-  var latlng = new google.maps.LatLng(x, y);
-  return latlng;
-}
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
 
-/**
- * Get current location
- * @param {} 
- * @return {} 
- */
-function getLocation(){
-  navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
-}
-
-/**
- * getCurrentPosition function succeeded in getting current location 
- * @param {object} pos   
- * @return {} 
- */
-function successCallback(pos){
-  var Potition_latitude = pos.coords.latitude;
-  var Potition_longitude = pos.coords.longitude;
-
-  // 位置情報が取得出来たらGoogle Mapを表示する
-  initialize(Potition_latitude,Potition_longitude);
-}
-
-/**
- * getCurrentPosition function failed in getting current location 
- * @param {object} error   
- * @return {} 
- */
-function errorCallback(error){
-
-}
-
-function redrawGoogleMap(map, marker,autocomplete){
-
-  marker.setVisible(false);
-  var place = autocomplete.getPlace();
-  if (!place.geometry) return;
-
-  // If the place has a geometry, then present it on a map.
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);
   }
 
-  marker.setPosition(place.geometry.location);
-  marker.setVisible(true);
-
-  var address = '';
-  if (place.address_components) {
-    address = [
-      (place.address_components[0] && place.address_components[0].short_name)
-      ].join(' ');
-  }
-
-}
-
-
+})(pm_map = pm_map || {});
 
