62abd50313d23f5900fc757fe17e7a78a47bd352
ikatsuki <ik2721@gmail.com>
2015-05-04 12:54:29 +0900

    Set markers on map in initial display

--- app/assets/javascripts/memos.js
+++ app/assets/javascripts/memos.js
@@ -1,7 +1,6 @@
 window.onload = function()
 {
-  // Get present location
-  pm_map.getLocation();
+  pm_map.initialize();
 
   // After place memo insert
   $('#memo_detail_places').on('cocoon:after-insert', function(e, insertedItem) {
@@ -27,107 +26,50 @@ var pm_map = pm_map || {}; // ancestor namespace
     };
   })();
 
-
   /**
    * Initialize Google Map
    * @param {Number} x latitude
    * @param {Number} y longitude 
    * @return {}
    */
-  function initialize(x, y) {
+  pm_map.initialize = function () {
 
     pm_map.map = new google.maps.Map(
       document.getElementById('map_canvas'), 
-      setOption(x,y));
+      setOption());
 
     pm_map.setAutoComplete(null);
+
   }
 
+  /**
+   * Set autocomplete feature to input fields
+   * @param {Object} insertedItem 
+   * @return {} 
+   */
   pm_map.setAutoComplete = function (insertedItem) {
  
     var $top = insertedItem ? $(insertedItem) : $('#contents');
+    var current_area = document.activeElement.closest('.collapsible-body');
 
-    // Set marker on present location
-    var marker = new google.maps.Marker({
-      map: pm_map.map,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
-
-    // Set autocomplete to address input field 
-    $top.find('.address').each(function(i,e){
-      var autocomplete = new google.maps.places.Autocomplete($(this)[0]);
+    $top.find('.collapsible-body').each(function(i,e){
+      // Set autocomplete to address input field 
+      var $address = $(this).find('.address')[0];
+      var autocomplete = new google.maps.places.Autocomplete($address);
       autocomplete.bindTo('bounds', pm_map.map);
 
+      // Set marker on map from existing place
+      var lat = $(this).find('[id$=latitude]').first().val() || null;
+      var lng = $(this).find('[id$=longitude]').first().val() || null;
+      var marker = createMarker(lat, lng);
+
+      setCenterPosition(lat, lng);
+ 
       google.maps.event.addListener(autocomplete, 'place_changed', function() {
         pm_map.redrawGoogleMap(marker, autocomplete);
       });
     });
   
-  }
-
-  /**
-   * Set options for Google Map
-   * @param {Number} x latitude
-   * @param {Number} y longitude 
-   * @return {Object} myOptions options for google map
-   */
-  function setOption(x, y){
-    var myOptions = {
-      zoom: 17,
-      center: new google.maps.LatLng(x, y),
-      mapTypeId: google.maps.MapTypeId.ROADMAP, 
-      disableDefaultUI: true,
-      panControl: true,
-      panControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_BOTTOM
-      },
-      zoomControl: true,
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_BOTTOM
-      }
-    };
-    return myOptions;
-  }
-
-  /**
-   * Create LatLng object 
-   * @param {Number} x  latitude
-   * @param {Number} y  longitude 
-   * @return {Object} latlng  LatLng object 
-   */
-  function setLatlng(x, y){
-    var latlng = new google.maps.LatLng(x, y);
-    return latlng;
-  }
-
-  /**
-   * Get current location
-   * @param {} 
-   * @return {} 
-   */
-  pm_map.getLocation = function (){
-    navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
-  }
-
-  /**
-   * getCurrentPosition function successed in getting current location 
-   * @param {object} pos   
-   * @return {} 
-   */
-  function successCallback(pos){
-    var Potition_latitude = pos.coords.latitude;
-    var Potition_longitude = pos.coords.longitude;
-
-    // 位置情報が取得出来たらGoogle Mapを表示する
-    initialize(Potition_latitude,Potition_longitude);
-  }
-
-  /**
-   * getCurrentPosition function failed in getting current location 
-   * @param {object} error   
-   * @return {} 
-   */
-  function errorCallback(error){
   }
 
   /**
@@ -155,6 +97,92 @@ var pm_map = pm_map || {}; // ancestor namespace
 
     // Set value to hidden field
     setLocationToHidden(location);
+  }
+
+  /**
+   * Create marker object
+   * @param {Number} x latitude
+   * @param {Number} y longitude 
+   * @return {Object} myOptions options for google map
+   */
+  function createMarker(lat, lng){
+    var latlng = (lat &&  lng) ? new google.maps.LatLng(lat, lng) : null;
+    return new google.maps.Marker({
+      map: pm_map.map,
+      anchorPoint: new google.maps.Point(0, -29),
+      position: latlng,
+      visible: true
+    });
+  }
+
+  /**
+   * Set options for Google Map
+   * @param {Number} x latitude
+   * @param {Number} y longitude 
+   * @return {Object} myOptions options for google map
+   */
+  function setOption(){
+    var myOptions = {
+      zoom: 17,
+      // center: new google.maps.LatLng(x, y),
+      mapTypeId: google.maps.MapTypeId.ROADMAP, 
+      disableDefaultUI: true,
+      panControl: true,
+      panControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      }
+    };
+    return myOptions;
+  }
+
+  /**
+   * Create LatLng object 
+   * @param {Number} x  latitude
+   * @param {Number} y  longitude 
+   * @return {Object} latlng  LatLng object 
+   */
+  function setLatlng(x, y){
+    var latlng = new google.maps.LatLng(x, y);
+    return latlng;
+  }
+
+  /**
+   * Set center postion on map
+   * @param {} 
+   * @return {} 
+   */
+  function setCenterPosition(lat, lng) {
+    if (lat && lng) {
+      pm_map.map.panTo(new google.maps.LatLng(lat, lng));
+    } else {
+      // Get present location
+      navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+    }
+  }
+
+  /**
+   * getCurrentPosition function successed in getting current location 
+   * @param {object} pos   
+   * @return {} 
+   */
+  function successCallback(pos){
+    var lat = pos.coords.latitude;
+    var lng = pos.coords.longitude;
+
+    // 位置情報が取得出来たらGoogle Mapを表示する
+    setCenterPosition(lat, lng);
+  }
+
+  /**
+   * getCurrentPosition function failed in getting current location 
+   * @param {object} error   
+   * @return {} 
+   */
+  function errorCallback(error){
   }
 
   /**
